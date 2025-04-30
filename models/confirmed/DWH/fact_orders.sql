bf4f2225fb59620eb4750c7067718424d8d2f1ed
sai sri <snowflaketraining48@gmail.com>
2025-04-30 03:25:39 +0000

    commit

--- models/confirmed/DWH/fact_orders.sql
+++ models/confirmed/DWH/fact_orders.sql
@@ -2,5 +2,5 @@
 
 WITH tb1 as(
     select * from
-{{source('snapshot_tables','snap_ordersdetails')}} where DBT_VALID_TO is null)
+{{ref('snap_ordersdetails')}} where DBT_VALID_TO is null)
 select * from tb1
--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -20,5 +20,5 @@ WITH tb1 as(
         when o_orderstatus = 'P' then 'Processing'
         end as order_status
 
-    from {{source('snowflake_data','RAW_ORDERS')}})
+    from {{source('snowflake_data','RAW_ORDER')}})
 select * from tb1
